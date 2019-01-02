e991ebaa4cfc13cb6019d972b68779c36869dbc1
mapleFU <1506118561@qq.com>
2019-01-02 23:08:56 +0800

    [Init] init gateway data

---
+++ src/main/java/cn/cloudbot/gateway/GatewayApplication.java
@@ -0,0 +1,48 @@
+package cn.cloudbot.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> testFunRouterFunction() {
+		RouterFunction<ServerResponse> route = RouterFunctions.route(
+				RequestPredicates.path("/testfun"),
+				request -> ServerResponse.ok().body(BodyInserters.fromObject("hello")));
+		return route;
+	}
+
+	@Bean
+	public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+		// @formatter:off
+		return builder.routes()
+				.route(r -> r.path("/test/**")
+//						.filters(f -> f.filter(new RequestTimeFilter())
+//								.addResponseHeader("X-Response-Default-Foo", "Default-Bar"))
+						.uri("http://localhost:8101/test/ip")
+
+				)
+				.build();
+		// @formatter:on
+	}
+
+}
+
