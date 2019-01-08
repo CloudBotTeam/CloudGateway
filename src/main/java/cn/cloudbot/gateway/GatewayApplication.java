8b105b9298a9e2c2fe56e723fbe7245de12b8b6d
ChenCyl <programming2@163.com>
2019-01-08 17:21:21 +0800

    modified:   src/main/java/cn/cloudbot/gateway/GatewayApplication.java

--- src/main/java/cn/cloudbot/gateway/GatewayApplication.java
+++ src/main/java/cn/cloudbot/gateway/GatewayApplication.java
@@ -7,12 +7,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+@CrossOrigin
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAutoConfiguration
