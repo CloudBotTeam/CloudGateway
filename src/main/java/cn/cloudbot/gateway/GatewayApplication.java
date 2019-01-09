1d16600d8edc208e8674984515f5f7622b0281e3
ChenCyl <programming2@163.com>
2019-01-09 22:02:45 +0800

    modified:   src/main/java/cn/cloudbot/gateway/GatewayApplication.java 	modified:   src/main/java/cn/cloudbot/gateway/controller/test.java 	modified:   src/main/resources/application.yml

--- src/main/java/cn/cloudbot/gateway/GatewayApplication.java
+++ src/main/java/cn/cloudbot/gateway/GatewayApplication.java
@@ -16,10 +16,10 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-@CrossOrigin
+//@CrossOrigin
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 public class GatewayApplication {
 
 	public static void main(String[] args) {
@@ -56,6 +56,5 @@ public class GatewayApplication {
 				.build();
 		// @formatter:on
 	}
-
 }
 
